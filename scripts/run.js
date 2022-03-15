abb809261fdd15898ea617ba118683f347501727
muritz <mauricechisom275@gmail.com>
2022-03-15 01:19:22 +0100

    stored data on blockchain 😜

--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,26 @@
 
 
 const main = async () => {
+    const [owner, jneDoe] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();// get total waves
+
+    let waveTxn = await waveContract.wave();// call d wave function
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();// get total waves again
+
+    waveTxn = await waveContract.connect(jneDoe).wave();// call d wave fn but this time using a random user gen address
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 };
 
 
